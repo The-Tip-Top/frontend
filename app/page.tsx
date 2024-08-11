c79554c7fa926e12974aa623d776b338532e5256
massi <massi@MacBook-Air-de-massi.local>
2024-08-11 18:00:35 +0200

    [Linter] add linter to the project
    closes:14-add-linter@trello

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen bg-red-600 flex-col items-center justify-between p-24">
       WELOCOME TO THE TIPTOP
-    </main>
+    </div>
   );
 }
