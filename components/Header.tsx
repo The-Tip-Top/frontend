978bae4fbdb48899920fc12799b266fd07c9e8f6
kebe96 <93620532+kebe96@users.noreply.github.com>
2024-08-20 22:02:36 +0200

    sauvegarde, robottxt, gooogle tag manager

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,96 +1,68 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { CircleUser, ContactIcon, HomeIcon, Menu, SquareUser, Search, LogOut } from "lucide-react"
-import { FaUser } from 'react-icons/fa'
-
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import NavBar from "@/components/Navbar"
+import { CircleUser, ContactIcon, HomeIcon, Menu, SquareUser, LogOut } from "lucide-react";
+import { FaUser } from 'react-icons/fa';
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import NavBar from "@/components/Navbar";
 import Image from 'next/image';
 import { useCurrentUser } from '@/lib/hooks/useCurrentUser';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import LogoutButton from '@/components/auth/logoutButton';
 
 const Header = () => {
-  const user = useCurrentUser()
+  const user = useCurrentUser();
+
+  useEffect(() => {
+    // Inject the Google Tag Manager script
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-DPV2VF21SS';
+    document.head.appendChild(script);
+
+    const scriptText = document.createElement('script');
+    scriptText.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-DPV2VF21SS');
+    `;
+    document.head.appendChild(scriptText);
+  }, []);
+
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 bg-white z-50 font-lato text-[#8FB43A] md:px-6">
       <title>Thé Tip Top - Votre Boutique de Thé Bio à Nice</title>
-      <meta name="description" content="Découvrez Thé Tip Top, votre expert en thé bio à Nice. Participez à notre jeu-concours exclusif et explorez nos thés relaxants pour le bien-être.
-        Nous ouvrons notre nouvelle boutique à Nice. Thétiptop Nice vous réserve de très belles surprises" />
-        <h1 className="sr-only">Thétiptop, c'est bien plus qu'une boutique de thé. Nous sommes une communauté de passionnés qui croit au pouvoir des ingrédients naturels 
-          pour le bien-être. Installée à Nice, notre boutique vous propose une expérience sensorielle unique autour du thé bio. Thétiptop Nice vous réserve de très belles surprises.
-          Découvrez Thétiptop Nice, votre nouvelle boutique de vente de thé bio à Nice et participez à notre jeu-concours exclusif ! Thétiptop est votre destination de choix pour découvrir
-          une vaste sélection de thés bio à Nice. Notre nouvelle boutique est située en plein cœur de la ville.
-          Nous avons des thés bio pour le bien-être, thé détox, thé minceur, thé vert, thé noir, thé blanc, thé rouge, thé oolong, thé matcha, thé bio en vrac, thé bio en sachet, thé bio en boîte.
-          Jeu concours Thétiptop, Bienvenue chez Thétiptop - Votre Expert en Thé Bio à Nice. Thétiptop, c'est bien plus qu'une boutique de thé. Nous sommes une communauté de passionnés qui croit au pouvoir des ingrédients naturels
-        </h1>
+      <meta name="description" content="Découvrez Thé Tip Top, votre expert en thé bio à Nice. Participez à notre jeu-concours exclusif et explorez nos thés relaxants pour le bien-être." />
+      <h1 className="sr-only">
+        Thétiptop, c'est bien plus qu'une boutique de thé. Nous sommes une communauté de passionnés qui croit au pouvoir des ingrédients naturels pour le bien-être...
+      </h1>
       <NavBar />
-      <Sheet >
+      <Sheet>
         <SheetTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="shrink-0 md:hidden"
-          >
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
             <Menu className="h-5 w-5" />
             <span className="sr-only">Toggle navigation menu</span>
           </Button>
         </SheetTrigger>
-        <SheetContent side="left" className='bg-white'>
+        <SheetContent side="left" className="bg-white">
           <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="#"
-              className="flex items-center gap-2 text-lg font-semibold"
-            >
-              <Image
-                src="/logofinal.png"
-                alt="Thétiptop, votre boutique de thé bio à Nice"
-                width={90}
-                height={90}
-                className="bg-red-500"
-              />
+            <Link href="#" className="flex items-center gap-2 text-lg font-semibold">
+              <Image src="/logofinal.png" alt="Thétiptop, votre boutique de thé bio à Nice" width={90} height={90} />
               <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Thé Tip Top</h1>
             </Link>
-
-            <Link
-              href="/home"
-              className="text-muted-foreground flex gap-6 transition-colors hover:text-foreground"
-            >
+            <Link href="/home" className="text-muted-foreground flex gap-6 transition-colors hover:text-foreground">
               <HomeIcon />
               Accueil
             </Link>
-            <Link
-              href="/home/contact"
-              className="text-muted-foreground flex gap-6  transition-colors hover:text-foreground"
-            >
+            <Link href="/home/contact" className="text-muted-foreground flex gap-6 transition-colors hover:text-foreground">
               <ContactIcon />
               Contact
             </Link>
-            <Link
-              href="/sign-up"
-              className="text-muted-foreground flex gap-6 items-center transition-colors hover:text-foreground"
-            >
+            <Link href="/sign-up" className="text-muted-foreground flex gap-6 items-center transition-colors hover:text-foreground">
               <SquareUser />
               Inscription
             </Link>
@@ -103,21 +75,21 @@ const Header = () => {
             <DropdownMenuTrigger asChild>
               <Avatar>
                 <AvatarImage src={user?.image || ""} />
-                <AvatarFallback className='bg-[#8FB43A] cursor-pointer'>
-                  <FaUser className='text-white' />
+                <AvatarFallback className="bg-[#8FB43A] cursor-pointer">
+                  <FaUser className="text-white" />
                 </AvatarFallback>
               </Avatar>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className='bg-white' align="end">
+            <DropdownMenuContent className="bg-white" align="end">
               <LogoutButton>
                 <DropdownMenuItem className="cursor-pointer">
-                  <LogOut className='h-4 w-4 mr-2' />
-                  Logout</DropdownMenuItem>
+                  <LogOut className="h-4 w-4 mr-2" />
+                  Logout
+                </DropdownMenuItem>
               </LogoutButton>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
-
       )}
     </header>
   );
