e829fc83cfe89980a409a8816d265a8678e9417a
YacineBOUHALA <docyacinou@gmail.com>
2024-08-30 20:38:59 +0200

    [Docker] test image

---
+++ app/admin/game-draw/page.tsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return (
+      <section className="flex-center size-full max-sm:px-6 flex-row">
+        <>test docker </>
+      </section>
+    );
+  };
+  
+  export default SignIn;
